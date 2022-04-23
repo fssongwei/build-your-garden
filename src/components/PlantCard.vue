6c745ad24ea2f512f7391df22d60859811be5cc3
cc2353 <cc2353@cornell.edu>
2022-04-23 21:30:54 +0000

    update cards

--- src/components/PlantCard.vue
+++ src/components/PlantCard.vue
@@ -2,7 +2,7 @@
   <b-col class="px-1">
       <b-card img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" style="max-width: 20rem;" class="mb-2">
     <b-card-text>
-      {{plantName}}
+     {{plantName}}
     </b-card-text>
     <b-button variant="primary">Plant Details</b-button>
   </b-card>
@@ -13,13 +13,15 @@
 import plantData from "@/data/fixtures.json";
 export default {
   name: "PlantCard",
-  components: {},
   props: {
-    plantName: String
+    plantName: {
+      type: String,
+      default: ""
+    }
   },
   data() {
     return {
-      plants: plantData,
+      plants: plantData
     };
   }
 };
--- src/views/Recommendations.vue
+++ src/views/Recommendations.vue
@@ -1,28 +1,33 @@
 <template>
   <b-container fluid class="main-view">
     <b-row class="my-2">
-      <b-col class="px-1"> <h1> 3. Recommended Plants </h1> </b-col>
-    </b-row>
-    <b-row class="my-2">
-      <plant-card plantName="Crocus"/>
-      <plant-card plantName="Parsley"/>
-      <plant-card plantName="Hibiscus"/>
-      <plant-card plantName="Petunia"/>
+      <PlantCard v-for="name in plantNames" :key="name" :plantName="name"/>
     </b-row>
     <b-row class="my-2">
       <b-col> <b-button pill variant="dark" size="lg">+</b-button> </b-col>
       <b-col></b-col>
       <b-col></b-col>
-      <b-col> <b-button variant="outline-dark" @click="$router.push('/garden')">See Garden</b-button> </b-col>
+      <b-col> <b-button variant="outline-dark" size="lg" @click="$router.push('/garden')">See Garden</b-button> </b-col>
     </b-row>
   </b-container>
 </template>
 
 <script>
 import PlantCard from "@/components/PlantCard.vue";
+import plantData from "@/data/fixtures.json";
+
+const {plantList} = plantData;
+const plantNames = Object.keys(plantList);
+console.log(plantNames);
 
 export default {
   name: "Recommendations",
   components: { PlantCard },
+  data() {
+    return {
+      plants: plantData,
+      plantNames,
+    }
+  }
 };
 </script>
