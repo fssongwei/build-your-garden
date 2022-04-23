1a563d41ee620fd387764f5cee5c779547a1e7da
cc2353 <cc2353@cornell.edu>
2022-04-23 18:47:35 +0000

    add recommendations branch

--- src/views/Recommendations.vue
+++ src/views/Recommendations.vue
@@ -1,9 +1,15 @@
 <template>
-  <div>
-    <!-- TODO: Implement the recommendation page -->
-    <h1>Recommendations Page</h1>
-    <plant-card />
-  </div>
+  <b-container fluid class="main-view">
+    <b-row class="my-2">
+      <b-col class="px-1"> <h1> 3. Recommended Plants </h1> </b-col>
+    </b-row>
+    <b-row class="my-2">
+      <plant-card plantName="Crocus"/>
+      <plant-card plantName="Parsley"/>
+      <plant-card plantName="Hibiscus"/>
+      <plant-card plantName="Petunia"/>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
