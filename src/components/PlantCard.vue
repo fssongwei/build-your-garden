63abfb149ccf21545e0421648e5d823345db13cb
cc2353 <cc2353@cornell.edu>
2022-04-25 14:56:14 +0000

    update plant card with images

--- src/components/PlantCard.vue
+++ src/components/PlantCard.vue
@@ -1,10 +1,14 @@
 <template>
   <b-col class="px-1">
-      <b-card img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" style="max-width: 20rem;" class="mb-2">
+      <b-card>
+        <b-card-img :src="`/img/garden/plants/${plantName}.png`" alt="Plant Image" img-height:100px></b-card-img>
     <b-card-text>
      {{plantName}}
     </b-card-text>
-    <b-button variant="primary">Plant Details</b-button>
+    <b-button id="details" variant="primary">Plant Details</b-button>
+    <b-popover target="details" title="More info" triggers="click blur" content="Placement bottom">
+      more plant information...
+    </b-popover>
   </b-card>
   </b-col>
 </template>
