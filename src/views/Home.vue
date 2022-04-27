6206c67ee7ec6fa955c7384d16245c0f1d6995a7
Wei Song <davidwsong@tencent.com>
2022-04-27 09:50:25 -0400

    update home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,69 +1,58 @@
 <template>
-  <div class="container">
+  <div class="home-container">
     <!-- TODO: Implement the home page -->
-    <h1>Home</h1>
 
-    <b-container class="bv-grid-row">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-md-4 col-md-offset-2">
-            <button
-              type="button"
-              class="btn btn-info btn-circle btn-lg"
-              @click="$router.push('/questions/set1/1')"
-            >
-              Step 1
-            </button>
-            <p>Plant Traits</p>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-md-4 col-md-offset-5">
-            <button
-              type="button"
-              class="btn btn-info btn-circle btn-lg"
-              @click="$router.push('/questions/set2/1')"
-            >
-              Step 2
-            </button>
-            <p>Garden logistics</p>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-md-4 col-md-offset-3">
-            <button
-              type="button"
-              class="btn btn-info btn-circle btn-lg"
-              @click="$router.push('/recommendations')"
-            >
-              Step 3
-            </button>
-            <p>See plant list</p>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-md-4 col-md-offset-6">
-            <button
-              type="button"
-              class="btn btn-info btn-circle btn-lg"
-              @click="$router.push('/garden')"
-            >
-              Step 4
-            </button>
-            <p>See Your Garden</p>
-          </div>
-        </div>
+    <div class="row-left">
+      <div class="step-btn-group">
+        <button class="circle-btn" @click="$router.push('/questions/set1/1')">
+          <span>Step</span><span>1</span>
+        </button>
+        <span>Plant Traits</span>
       </div>
-    </b-container>
+    </div>
+
+    <div class="row-center">
+      <img src="/img/dialog.png" />
+    </div>
+
+    <div class="row-right">
+      <div class="step-btn-group">
+        <button class="circle-btn" @click="$router.push('/questions/set2/1')">
+          <span>Step</span><span>2</span>
+        </button>
+        <span>Garden Logistics</span>
+      </div>
+    </div>
+
+    <div class="row-center">
+      <img src="/img/anti-dialog.png" />
+    </div>
+
+    <div class="row-left">
+      <div class="step-btn-group">
+        <button class="circle-btn" @click="$router.push('/recommendations')">
+          <span>Step</span><span>3</span>
+        </button>
+        <span>See Plant List</span>
+      </div>
+    </div>
+
+    <div class="row-center">
+      <img src="/img/dialog.png" />
+    </div>
+
+    <div class="row-right">
+      <div class="step-btn-group">
+        <button class="circle-btn" @click="$router.push('/garden')">
+          <span>Step</span><span>4</span>
+        </button>
+        <span>See Your Garden</span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
 export default {
   name: "Home",
   components: {},
@@ -77,44 +66,50 @@ export default {
 </script>
 
 <style>
-.col-md-4 {
-  float: right;
-}
-.left {
-  display: inline;
-  float: left;
-}
-.col-md-offset-3 {
-  margin-left: 25%;
-}
-.col-md-offset-5 {
-  margin-left: 42.66%;
-}
-.col-md-offset-6 {
-  margin-left: 50%;
-}
-
-.col-md-offset-2 {
-  margin-left: 16.66%;
-}
-.right {
-  float: right;
-}
-.btn-circle {
-  width: 30px;
-  height: 30px;
-  text-align: center;
-  padding: 20px 0;
-  font-size: 12px;
-  line-height: 1.428571429;
-  border-radius: 15px;
-}
-.btn-circle.btn-lg {
+.circle-btn {
   width: 80px;
   height: 80px;
-  padding: 20px 16px;
   font-size: 18px;
-  line-height: 1.33;
-  border-radius: 50px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: #0dcaf0;
+  border: none;
+}
+
+.step-btn-group {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.home-container {
+  width: 100%;
+  max-width: 550px;
+  margin: 0 auto;
+  padding: 80px 50px;
+}
+
+.row-left {
+  display: flex;
+  justify-content: flex-start;
+}
+
+.row-right {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.row-center {
+  width: 100%;
+  box-sizing: border-box;
+  padding: 0 60px 10px 40px;
+}
+
+.row-center img {
+  width: 100%;
 }
 </style>
