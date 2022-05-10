178f881a4e2b6012d890390f9d66efca3b4a2eb7
Wei Song <davidwsong@tencent.com>
2022-05-10 15:02:23 -0400

    fixed landing page layout

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,35 +1,56 @@
 <template>
   <div class="container">
-      <b-row class="mx-4 my-3" style="display: flex; flex-direction: row; justify-content: center">
-        <b-col style="width: 100%; max-width: 30rem;">
-          <b-card class="text-center" img-src="img/icons/flowermascot.png" img-alt="mascot image" img-right>
-            <b-card-text class="mt-4" style="font-size: 24px"> <b>Hello!</b> </b-card-text>
-            <b-card-text style="font-size: 18px"> My name is Flory. Nice to meet you! </b-card-text>
-          </b-card>
-        </b-col>
-      </b-row>
+    <div class="d-flex justify-content-center mx-4 my-4">
+      <div
+        class="d-flex justify-content-between align-itens-center"
+        style="
+          width: 100%;
+          max-width: 450px;
+          border: 1px solid rgba(0, 0, 0, 0.125);
+          border-radius: 5px;
+        "
+      >
+        <div
+          class="d-flex flex-column align-items-center justify-content-center"
+        >
+          <b style="font-size: 24px"> Hello! </b>
+          <span style="font-size: 18px; text-align: center" class="mt-4">
+            My name is Flory. Nice to meet you!
+          </span>
+        </div>
 
-      <b-row class="mx-4 my-4">
-        <b-col class="text-center">
-          <h3>Welcome to Flory's Garden!</h3>
-        </b-col>
-      </b-row>
+        <img src="@/data/flowermascot.png" class="landing-mascot" />
+      </div>
+    </div>
 
-      <b-row style="justify-content: center">
-        <b-col cols="9">
-          <p style="font-size:20px">Flory's Garden is a personalized garden planning assistant that helps you create your dream garden with a Cornell University Playful Plants database! </p>
-          <p style="font-size:20px"> Watch your garden come to life through our personalized plant recommendations and garden visualizer. We hope to make your garden planning process as customized and effortless as possible!</p>
-        </b-col>
-      </b-row>
+    <b-row class="mx-4 my-4">
+      <b-col class="text-center">
+        <h3>Welcome to Flory's Garden!</h3>
+      </b-col>
+    </b-row>
 
-      <b-row class="mx-5 my-4" style="justify-content: center">
-        <b-col class="text-center">
-            <router-link to="/">
-              <b-button size="lg" variant="success"> START! </b-button>
-            </router-link>
-        </b-col>
-      </b-row>
+    <b-row style="justify-content: center">
+      <b-col cols="9">
+        <p style="font-size: 20px">
+          Flory's Garden is a personalized garden planning assistant that helps
+          you create your dream garden with a Cornell University Playful Plants
+          database!
+        </p>
+        <p style="font-size: 20px">
+          Watch your garden come to life through our personalized plant
+          recommendations and garden visualizer. We hope to make your garden
+          planning process as customized and effortless as possible!
+        </p>
+      </b-col>
+    </b-row>
 
+    <b-row class="mx-5 my-4" style="justify-content: center">
+      <b-col class="text-center">
+        <router-link to="/">
+          <b-button size="lg" variant="success"> START! </b-button>
+        </router-link>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
@@ -38,13 +59,11 @@
 
 export default {
   name: "Landing",
-  components: {
-
-  },
+  components: {},
 };
 </script>
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500&display=swap");
 
 /* .container {
   max-width: 500px;
@@ -55,6 +74,17 @@ export default {
 } */
 
 .card-img-right {
- max-width: 15rem;
+  max-width: 15rem;
+}
+
+.landing-mascot {
+  width: 250px;
+}
+
+@media screen and (max-width: 576px) {
+  .landing-mascot {
+    width: 150px;
+    height: 150px;
+  }
 }
 </style>
